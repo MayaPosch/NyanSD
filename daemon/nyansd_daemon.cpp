f2cd51870522fe09432f9eb3770c7b3d153d7943
Maya Posch <maya@nyanko.ws>
2020-06-26 13:15:56 +0200

    Add Windows Service support.

--- daemon/nyansd_daemon.cpp
+++ daemon/nyansd_daemon.cpp
@@ -30,14 +30,47 @@ void signal_handler(int signal) {
 }
 
 
+#ifdef _WIN32
+#include <windows.h>
+#include <strsafe.h>
+
+//#define SVCNAME TEXT("NyanSD")
+const char* SVCNAME = { "NyanSD" };
+
+SERVICE_STATUS          gSvcStatus; 
+SERVICE_STATUS_HANDLE   gSvcStatusHandle; 
+HANDLE                  ghSvcStopEvent = 0;
+
+VOID WINAPI SvcCtrlHandler(DWORD);
+VOID WINAPI SvcMain(DWORD, LPTSTR*);
+
+VOID ReportSvcStatus(DWORD, DWORD, DWORD);
+//VOID SvcInit(DWORD, LPTSTR*); 
+VOID SvcReportEvent(LPTSTR);
+
+void svcInit() {
+#else
 int main() {
+#endif
 	// Add the service records.
 	// Get the list of files in the 'services' folder, parsing all 'service' files'
 #ifdef _WIN32
+	ghSvcStopEvent = CreateEvent(
+							NULL,    // default security attributes
+							TRUE,    // manual reset event
+							FALSE,   // not signaled
+							NULL);   // no name
+							
+	if (ghSvcStopEvent == NULL) {
+        ReportSvcStatus(SERVICE_STOPPED, NO_ERROR, 0);
+        return;
+    }
+						 
 	char* pdpath = getenv("ProgramData");
 	if (pdpath == nullptr) {
 		std::cerr << "Could not find 'ProgramData' environment variable." << std::endl;
-		return 1;
+		ReportSvcStatus(SERVICE_STOPPED, NO_ERROR, 0);
+		return;
 	}
 	
 	std::string fullpath(pdpath);
@@ -49,7 +82,12 @@ int main() {
 
 	if (!fs::exists(path)) {
 		std::cerr << "Services path " << path << " does not exist." << std::endl;
+#ifdef _WIN32
+		ReportSvcStatus(SERVICE_STOPPED, NO_ERROR, 0);
+		return;
+#else
 		return 1;
+#endif
 	}
 	
 	for (fs::directory_entry const &entry : fs::directory_iterator(path)) {
@@ -57,7 +95,12 @@ int main() {
 			INIReader serviceFile(entry.path().string());	// Read in service file.
 			if (serviceFile.ParseError() != 0) {
 				std::cerr << "Failed to parse file '" << path << "'." << std::endl;
-				return false;
+#ifdef _WIN32
+				ReportSvcStatus(SERVICE_STOPPED, NO_ERROR, 0);
+				return;
+#else
+				return 1;
+#endif
 			}
 			
 			std::set<std::string> sections = serviceFile.Sections();
@@ -100,7 +143,12 @@ int main() {
 				
 				if (!NyanSD::addService(sv)) { 
 					std::cerr << "Failed to add service. Aborting..." << std::endl;
+#ifdef _WIN32
+					ReportSvcStatus(SERVICE_STOPPED, NO_ERROR, 0);
+					return;
+#else
 					return 1;
+#endif
 				}
 			}
 		}
@@ -115,6 +163,10 @@ int main() {
 	std::cout << "Starting server..." << std::endl;
 	NyanSD::startListener(11310);
 	
+#ifdef _WIN32
+	ReportSvcStatus(SERVICE_RUNNING, NO_ERROR, 0);
+#endif
+	
 	// Wait for signal.
 	std::cout << "Running... terminate with SIGINT (Ctrl+c)." << std::endl;
 	std::unique_lock<std::mutex> lk(gMutex);
@@ -126,5 +178,200 @@ int main() {
 	
 	std::cout << "All done." << std::endl;
 	
+#ifdef _WIN32
+	return;
+#else
 	return 0;
+#endif
 }
+
+
+#ifdef _WIN32
+//
+// Purpose: 
+//   Entry point for the process
+//
+// Parameters:
+//   None
+// 
+// Return value:
+//   None, defaults to 0 (zero)
+//
+int __cdecl main(int argc, TCHAR *argv[])  { 
+    // If command-line parameter is "install", install the service. 
+    // Otherwise, the service is probably being started by the SCM.
+    /* if (lstrcmpi( argv[1], TEXT("install")) == 0) {
+        SvcInstall();
+        return;
+    } */
+
+    // TO_DO: Add any additional services for the process to this table.
+    SERVICE_TABLE_ENTRY DispatchTable[] =  { 
+        { (LPTSTR) SVCNAME, (LPSERVICE_MAIN_FUNCTION) SvcMain }, 
+        { NULL, NULL } 
+    }; 
+ 
+    // This call returns when the service has stopped. 
+    // The process should simply terminate when the call returns.
+    if (!StartServiceCtrlDispatcher(DispatchTable)) { 
+		const char* ssvc = { "StartServiceCtrlDispatcher" };
+        SvcReportEvent((LPTSTR) ssvc);
+    } 
+	
+	return 0;
+} 
+
+
+//
+// Purpose: 
+//   Entry point for the service
+//
+// Parameters:
+//   dwArgc - Number of arguments in the lpszArgv array
+//   lpszArgv - Array of strings. The first string is the name of
+//     the service and subsequent strings are passed by the process
+//     that called the StartService function to start the service.
+// 
+// Return value:
+//   None.
+//
+void WINAPI SvcMain( DWORD dwArgc, LPTSTR* lpszArgv) {
+    // Register the handler function for the service
+
+    gSvcStatusHandle = RegisterServiceCtrlHandler(SVCNAME, SvcCtrlHandler);
+
+    if (!gSvcStatusHandle) { 
+		const char* rsvc = { "RegisterServiceCtrlHandler" };
+        SvcReportEvent((LPTSTR) rsvc); 
+        return; 
+    } 
+
+    // These SERVICE_STATUS members remain as set here
+    gSvcStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS; 
+    gSvcStatus.dwServiceSpecificExitCode = 0;    
+
+    // Report initial status to the SCM
+    ReportSvcStatus(SERVICE_START_PENDING, NO_ERROR, 3000);
+
+    // Perform service-specific initialization and work.
+    //SvcInit( dwArgc, lpszArgv );
+	svcInit();
+	
+	ReportSvcStatus(SERVICE_STOPPED, NO_ERROR, 0);
+}
+
+
+//
+// Purpose: 
+//   Called by SCM whenever a control code is sent to the service
+//   using the ControlService function.
+//
+// Parameters:
+//   dwCtrl - control code
+// 
+// Return value:
+//   None
+//
+void WINAPI SvcCtrlHandler(DWORD dwCtrl) {
+   // Handle the requested control code. 
+
+   switch(dwCtrl) {  
+      case SERVICE_CONTROL_STOP: 
+         ReportSvcStatus(SERVICE_STOP_PENDING, NO_ERROR, 0);
+
+         // Signal the service to stop.
+         //SetEvent(ghSvcStopEvent);
+		 gCv.notify_all();
+         ReportSvcStatus(gSvcStatus.dwCurrentState, NO_ERROR, 0);
+         
+         return;
+ 
+      case SERVICE_CONTROL_INTERROGATE: 
+         break; 
+ 
+      default: 
+         break;
+   }
+}
+
+
+//
+// Purpose: 
+//   Sets the current service status and reports it to the SCM.
+//
+// Parameters:
+//   dwCurrentState - The current state (see SERVICE_STATUS)
+//   dwWin32ExitCode - The system error code
+//   dwWaitHint - Estimated time for pending operation, 
+//     in milliseconds
+// 
+// Return value:
+//   None
+//
+VOID ReportSvcStatus(DWORD dwCurrentState, DWORD dwWin32ExitCode, DWORD dwWaitHint) {
+    static DWORD dwCheckPoint = 1;
+
+    // Fill in the SERVICE_STATUS structure.
+    gSvcStatus.dwCurrentState = dwCurrentState;
+    gSvcStatus.dwWin32ExitCode = dwWin32ExitCode;
+    gSvcStatus.dwWaitHint = dwWaitHint;
+
+    if (dwCurrentState == SERVICE_START_PENDING) { gSvcStatus.dwControlsAccepted = 0; }
+    else { gSvcStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP; }
+
+    if ((dwCurrentState == SERVICE_RUNNING) || (dwCurrentState == SERVICE_STOPPED)) {
+        gSvcStatus.dwCheckPoint = 0;
+	}
+    else {
+		gSvcStatus.dwCheckPoint = dwCheckPoint++;
+	}
+
+    // Report the status of the service to the SCM.
+    SetServiceStatus(gSvcStatusHandle, &gSvcStatus);
+}
+
+
+#define SVC_ERROR                        ((DWORD)0xC0020001L)
+
+
+//
+// Purpose: 
+//   Logs messages to the event log
+//
+// Parameters:
+//   szFunction - name of function that failed
+// 
+// Return value:
+//   None
+//
+// Remarks:
+//   The service must have an entry in the Application event log.
+//
+VOID SvcReportEvent(LPTSTR szFunction) { 
+    HANDLE hEventSource;
+    LPCTSTR lpszStrings[2];
+    TCHAR Buffer[80];
+
+    hEventSource = RegisterEventSource(NULL, SVCNAME);
+
+    if (NULL != hEventSource) {
+        StringCchPrintf(Buffer, 80, TEXT("%s failed with %d"), szFunction, GetLastError());
+
+        lpszStrings[0] = SVCNAME;
+        lpszStrings[1] = Buffer;
+
+        ReportEvent(hEventSource,        // event log handle
+                    EVENTLOG_ERROR_TYPE, // event type
+                    0,                   // event category
+                    SVC_ERROR,           // event identifier
+                    NULL,                // no security identifier
+                    2,                   // size of lpszStrings array
+                    0,                   // no binary data
+                    lpszStrings,         // array of strings
+                    NULL);               // no binary data
+
+        DeregisterEventSource(hEventSource);
+    }
+}
+
+#endif
