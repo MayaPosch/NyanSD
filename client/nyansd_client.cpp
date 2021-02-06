47614d819778927b4de3cca7e62209fd8d0b668f
Maya Posch <maya@nyanko.ws>
2021-02-06 11:46:41 +0100

    Add duplicate filtering feature. Increase reporting.

--- client/nyansd_client.cpp
+++ client/nyansd_client.cpp
@@ -10,6 +10,22 @@
 #include <iostream>
 
 
+bool isDuplicate(std::vector<NYSD_service> &uniques, NYSD_service &rm) {
+	for (uint32_t j = 0; j < uniques.size(); ++j) {
+		if (uniques[j].ipv4 == rm.ipv4 &&
+			uniques[j].ipv6 == rm.ipv6 &&
+			uniques[j].hostname == rm.hostname &&
+			uniques[j].port == rm.port &&
+			uniques[j].service == rm.service &&
+			uniques[j].protocol == rm.protocol) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+
 int main() {
 	// Send query via NyanSD.
 	std::vector<NYSD_query> queries;
@@ -22,13 +38,34 @@ int main() {
 	std::cout << "Sending query..." << std::endl;
 	NyanSD::sendQuery(11310, queries, responses);
 	
-	// Print out responses.
 	std::cout << "Received " << responses.size() << " responses." << std::endl;
+	
+	// Filter out duplicates.
+	std::vector<NYSD_service> uniques;
 	for (int i = 0; i < responses.size(); ++i) {
-		std::cout << "Got service " << responses[i].service << " on host " << responses[i].hostname
-					<< ", IPv4 " << NyanSD::ipv4_uintToString(responses[i].ipv4) 
-					<< ", IPv6 " << responses[i].ipv6
-					<< ", port " << (uint16_t) responses[i].port << std::endl;
+		if (isDuplicate(uniques, responses[i])) {
+			std::cout << "Skipping duplicate entry for '" << responses[i].service << "'" << std::endl;
+			continue;
+		}
+		
+		uniques.push_back(responses[i]);
+	}
+	
+	// Print out responses.
+	for (uint32_t i = 0; i < uniques.size(); ++i) {
+		std::cout << "Got service " << uniques[i].service << " on host " << uniques[i].hostname
+					<< ", IPv4 " << NyanSD::ipv4_uintToString(uniques[i].ipv4) 
+					<< ", IPv6 " << uniques[i].ipv6
+					<< ", port " << (uint16_t) uniques[i].port;
+		if (uniques[i].protocol == NYSD_PROTOCOL_TCP) {
+			std::cout << " (TCP)" << std::endl;
+		}
+		else if (uniques[i].protocol == NYSD_PROTOCOL_UDP) {
+			std::cout << " (UDP)" << std::endl;
+		}
+		else {
+			std::cout << " (ALL)" << std::endl;
+		}
 	}
 	
 	std::cout << "All done." << std::endl;
